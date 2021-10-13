b23eeccf5ca7aad6782f9c136237c4201e09d777
shrey <shrey.singhal.com>
2021-10-13 13:13:57 +0530

    Finally product is adding to cart

---
+++ index.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+
+const addToCart = {
+  productlinks: `//*[@class='product-image _deals-shoveler-v2_style_dealImage__3nlqg']`,
+  addbtn: "input#add-to-cart-button",
+  crtbtn: "div#nav-cart-count-container",
+};
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {
+      width: 1500,
+      height: 1000,
+    },
+    args: ["--start-fullscreen"],
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.amazon.in/");
+  await page.waitForXPath(addToCart.productlinks);
+
+  const product = await page.$x(addToCart.productlinks);
+  await product[1].click();
+
+  await page.waitForSelector(addToCart.addbtn);
+  await page.click(addToCart.addbtn);
+  await page.waitForSelector(addToCart.crtbtn);
+  await page.click(addToCart.crtbtn);
+})();
