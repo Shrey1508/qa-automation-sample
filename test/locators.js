a107e5dd9eccdfe70debfe80778fe10cf9e6cb05
shrey <shrey.singhal.com>
2021-10-14 18:16:01 +0530

    using mocha framework and refactor the script

---
+++ test/locators.js
@@ -0,0 +1,10 @@
+const login = {
+  lgnbtn: "div.nav-line-1-container",
+  username: "input#ap_email",
+  cntbtn: "input#continue",
+  pass: "input#ap_password",
+  signIn: "input#signInSubmit",
+};
+
+//export default login;
+module.exports = { login: login };
