29c220a9e33e4430a158b494cb0b0939302c4784
shrey <shrey.singhal.com>
2021-10-26 18:35:42 +0530

    multiple browser supporting and incognito insstance

--- amazonfeature.js
+++ amazonfeature.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const puppeteer = require("puppeteer");
+const puppeteerchrome = require("puppeteer");
+const puppeteerfirefox = require("puppeteer-firefox");
 
 const { USER_NAME, PASSWORD, BASE_URL } = process.env;
 
@@ -13,14 +14,30 @@ const login = {
 };
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(BASE_URL);
-  await page.click(login.lgnbtn);
-  await page.waitForSelector(login.email);
-  await page.type(login.email, USER_NAME);
-  await page.click(login.cntbtn);
-  await page.waitForSelector(login.pass);
-  await page.type(login.pass, PASSWORD);
-  await page.click(login.signIn);
+  const test = async (browser) => {
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.goto(BASE_URL);
+    await page.click(login.lgnbtn, { setTimeout: 5000 });
+    await page.waitForSelector(login.email);
+    await page.type(login.email, USER_NAME);
+    await page.click(login.cntbtn);
+    await page.waitForSelector(login.pass);
+    await page.type(login.pass, PASSWORD);
+    await page.click(login.signIn);
+    browser.close();
+  };
+
+  const chrome = await puppeteerchrome.launch({
+    headless: false,
+  });
+
+  await test(chrome);
+
+  const firefox = await puppeteerfirefox.launch({
+    headless: false,
+    args: ["-private"],
+  });
+
+  await test(firefox);
 })();
---
+++ new.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const puppeteer = require("puppeteer");
+//const puppeteerfirefox = require("puppeteer-firefox");
+
+const { USER_NAME, PASSWORD, BASE_URL } = process.env;
+
+const login = {
+  lgnbtn: "div.nav-line-1-container",
+  email: "input#ap_email",
+  cntbtn: "input#continue",
+  pass: "input#ap_password",
+  signIn: "input#signInSubmit",
+};
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    //product: "firefox",
+  });
+
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  //const page = await browser.newPage();
+  await page.goto(BASE_URL);
+  await page.click(login.lgnbtn);
+  await page.waitForSelector(login.email);
+  await page.type(login.email, USER_NAME);
+  await page.click(login.cntbtn);
+  await page.waitForSelector(login.pass);
+  await page.type(login.pass, PASSWORD);
+  await page.click(login.signIn);
+})();
